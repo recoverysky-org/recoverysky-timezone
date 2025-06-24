4eb8f14c7bf0e6bc2bc5de4662bc430fe551ea40
Jenova Marie <jenova@recoverysky.org>
2025-06-24 01:46:29 -0500

    Update tsup configuration to support multiple entry points and enhance build settings
    - Changed entry points to include all TypeScript files in the server and common directories.
    - Added output extension customization for JavaScript files.
    - Disabled code splitting and tree shaking for the build process.
    - Specified external dependencies to optimize the build output.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/server/server.ts'],
+  entry: ['src/server/**/*.ts', 'src/common/**/*.ts'],
   outDir: 'dist',
   format: ['cjs'], // 👈 CommonJS output
   target: 'node22',
   sourcemap: true,
   clean: true,
   dts: false, // or true if you want `.d.ts`
+  splitting: false,
+  treeshake: false,
+  external: ['luxon', 'timezonecomplete', 'express', 'lodash', 'dotenv', 'ts-rust-result'],
+  outExtension({ format }) {
+    return {
+      js: '.js',
+    }
+  },
 })
