237f32cf82d032cfb444c13f6f45650e24fbebf8
Jenova Marie <jenova@recoverysky.org>
2025-06-24 23:12:10 -0500

    Add toTz function for timezone conversion from ISO strings
    - Introduced a new function, toTz, to convert an ISO datetime string from a specified IANA timezone to UTC.
    - Implemented parameter validation to ensure required inputs are provided.
    - Utilized the timezonecomplete library for accurate timezone handling and conversion.
    - Enhanced error handling to return meaningful error messages in case of invalid inputs.

---
+++ src/server/functions/to-tz.ts
@@ -0,0 +1,34 @@
+import { DateTime, zone } from 'timezonecomplete';
+import { assertNotNil, err, ok, Result } from 'ts-rust-result';
+
+/**
+ * Converts an ISO datetime string to UTC
+ * @param isoString - The ISO datetime string (timezone is ignored)
+ * @param sourceTimeZone - The IANA timezone to convert to convert from
+ * @param targetTimeZone - The IANA timezone to convert to (defaults to UTC)
+ * @returns Promise<Result<{ iso: string; millis: number }>> - ISO string and milliseconds or error
+ */
+export async function toTz(isoString: string, sourceTimeZone?: string, targetTimeZone?: string ): Promise<Result<{ iso: string; millis: number }>> {
+  try {
+    assertNotNil(isoString, 'isoString is required'); 
+    assertNotNil(sourceTimeZone, 'sourceTimeZone is required'); 
+
+    // Set defaults
+    targetTimeZone = targetTimeZone || 'UTC';
+
+    // Parse the input ISO string in the source time zone
+    let sourceTz = zone(sourceTimeZone!);
+    const source = new DateTime(isoString, sourceTz);
+
+    // Convert to target time zone
+    let destTz = zone(targetTimeZone!);
+    const target = source.toZone(destTz)
+
+    return ok({
+      iso: target.toIsoString(), // Full ISO string with offset
+      millis: target.unixUtcMillis()
+    });
+  } catch (error) {
+      return err(error as Error);
+  }
+}
